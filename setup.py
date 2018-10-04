c9a6559a34e070e7397f63c3019ab7a1e80d1f20
dmitrii <dmitriilazukov@gmail.com>
2018-10-04 15:20:11 +0500

    Add base setup code for packaging.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='bx24_orm',
+      version='0.1',
+      description='Easy to use Django-styled API wrapper for bitrix 24',
+      url='https://github.com/dmitriilazukov/bx24_orm',
+      author='Dmitrii Lazukov',
+      author_email='dmitriilazukov@gmail.com',
+      license='MIT',
+      packages=['bx24_orm'],
+      zip_safe=False)
