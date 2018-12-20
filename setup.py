407912872c9a66f44f8742448f7c2fc3fc672120
dmitrii <dmitriilazukov@gmail.com>
2018-12-20 22:20:44 +0500

    Add LICENSE. Update setup.py

--- setup.py
+++ setup.py
@@ -1,15 +1,17 @@
 from setuptools import setup, find_packages
 
-PACKAGES = find_packages(where='.', exclude='bx24_settings')
-
+with open("README.md", "r") as fh:
+    long_description = fh.read()
 setup(name='bx24_orm',
       version='0.0.1',
       description='Easy to use Django-styled API wrapper for Bitrix 24',
+      long_description=long_description,
+      long_description_content_type="text/markdown",
       url='https://github.com/dmitriilazukov/bx24_orm',
       author='Dmitrii Lazukov',
       author_email='dmitriilazukov@gmail.com',
       license='MIT',
-      packages=PACKAGES,
+      packages=find_packages(where='.', exclude='bx24_settings'),
       entry_points={
           'console_scripts': ['bx24_cmd=bx24_orm.scripts.command_line:bx24_cmd'],
       },
@@ -17,5 +19,10 @@ setup(name='bx24_orm',
           'requests',
           'six',
           'python-dateutil'
+      ],
+      classifiers=[
+          "Programming Language :: Python :: 2.7",
+          "License :: OSI Approved :: MIT License",
+          "Operating System :: OS Independent",
       ],
       zip_safe=False)
